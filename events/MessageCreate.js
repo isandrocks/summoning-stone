9240c4a4aabfb1556b357bc588640ecfc221a90f
iSandRocks <sean.h1313@gmail.com>
2023-07-18 18:53:51 -0400

    more typos

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -1,6 +1,6 @@
 const { Events, } = require('discord.js');
 const { summonid, modid } = require('../config.json');
-const allMembers = require('./db/members.json');
+const allMembers = require('../db/members.json');
 const members = allMembers.members;
 
 
