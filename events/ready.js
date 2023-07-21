c0a748c85cfdd422a3a635a69a1a2055297b2e0e
iSandRocks <sean.h1313@gmail.com>
2023-07-21 16:39:00 -0400

    trying to get it to delete first then post the msg

--- events/ready.js
+++ events/ready.js
@@ -7,8 +7,9 @@ module.exports = {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		try {
 			// console.log(client);
-			client.commands.get('clearchannel').execute(client);
-			// client.commands.get('summonMessage').execute(client);
+			client.commands.get('clearchannel').execute(client)
+				.then(client.commands.get('summon-msg').execute(client));
+
 		} catch (error) {
 			console.error(`ERROR FROM READY EVENT: ${error}`);
 		}
